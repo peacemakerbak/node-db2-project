10eeb29d276b084c500a1b8e027544490f12d43c
peacemakerbak <peacemakerbak@gmail.com>
2024-08-07 15:59:17 -0400

    vin fixed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+      vin: "1C3CDFBA6DD197561",
+      make: "toyota",
+      model: "prius",
+      mileage: 215000,
+      title: "clean",
+      transmission: "manual",
+    },
+    {
+      vin: "JM1BK143651274706",
+      make: "toyota",
+      model: "corolla",
+      mileage: 115000,
+      title: "salvage",
+    },
+    {
+      vin: "3N1AB6AP0BL727208",
+      make: "ford",
+      model: "focus",
+      mileage: 15000,
+    },
+  ];
+  
+ 
+  
+  exports.seed = async function(knex){
+      await knex('cars').truncate()
+      await knex('cars').insert(cars)
+  }
